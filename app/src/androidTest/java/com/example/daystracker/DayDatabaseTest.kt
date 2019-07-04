7a92bfb917ebe1c751a46bd134b0eec250e24e00
Denny Casabianca <casabiancadenny@gmail.com>
2019-07-04 17:06:44 +0200

    Added Room DB test

---
+++ app/src/androidTest/java/com/example/daystracker/DayDatabaseTest.kt
@@ -0,0 +1,50 @@
+package com.example.daystracker
+
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import com.example.daystracker.database.DayDatabase
+import com.example.daystracker.database.DayDatabaseDao
+import org.junit.runner.RunWith
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.daystracker.database.Day
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+@RunWith(AndroidJUnit4::class)
+class DayDatabaseTest {
+
+    private lateinit var dayDao: DayDatabaseDao
+    private lateinit var db: DayDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, DayDatabase::class.java)
+            // Allowing main thread queries, just for testing.
+            .allowMainThreadQueries()
+            .build()
+        dayDao = db.dayDatabaseDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun insertAndGetDay() {
+        val day = Day()
+        dayDao.insert(day)
+        val today = dayDao.getAllDays()
+        assertEquals(today.value?.get(0)?.dayNumber,null)
+    }
+
+}
